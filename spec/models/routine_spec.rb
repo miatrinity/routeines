45c518c50fb949f6676b3c200c6e3eec52ff837b
Mia Sinek && Trinity Takei <m14tr1n1ty@gmail.com>
2020-02-04 18:25:59 +0100

    Finish implementation of Routine model by TDD-ing fields/avatar attachment

--- spec/models/routine_spec.rb
+++ spec/models/routine_spec.rb
@@ -3,11 +3,16 @@
 require 'rails_helper'
 
 RSpec.describe Routine, type: :model do
+  describe '#title' do
+    it { should validate_presence_of(:title) }
+  end
+
+  describe '#avatar' do
+    it { is_expected.to validate_attachment_of(:avatar) }
+  end
+
   describe 'associations' do
     it { should belong_to(:user) }
-
-    describe '#avatar' do
-      it { is_expected.to validate_attachment_of(:avatar) }
-    end
   end
 end
+
