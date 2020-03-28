332827e5a49d205fc4945339a5634e5e167df339
Mia Sinek && Trinity Takei <m14tr1n1ty@gmail.com>
2020-03-28 15:39:54 +0100

    Update delete realted specs

--- spec/system/user/steps/delete_spec.rb
+++ spec/system/user/steps/delete_spec.rb
@@ -4,18 +4,19 @@ require 'rails_helper'
 
 RSpec.describe 'User deletes step', type: :system do
   it 'successfully' do
-    step = create(:step, first: true)
+    routine = create(:routine, :with_steps)
+    step = routine.steps.sample
 
-    login_as step.routine.user
+    login_as routine.user
 
-    visit routine_steps_path(step.routine)
+    visit routine_steps_path(routine)
 
     accept_confirm do
       find("#step_#{step.id} [aria-label='delete']").click
     end
 
     expect(page).to have_text('Step was successfully deleted.')
-    expect(page).to_not have_css('[id^=step_]')
-    expect(page).to have_css('#step-count', text: '0')
+    expect(page).to have_css('[id^=step_]', count: 2)
+    expect(page).to have_css('#step-count', text: '2')
   end
 end
