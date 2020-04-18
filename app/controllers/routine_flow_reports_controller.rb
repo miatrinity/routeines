67d02bd321680c82fb1473e9d292dafe8766993f
Mia Sinek && Trinity Takei <m14tr1n1ty@gmail.com>
2020-04-18 11:25:57 +0200

    Move ordering to routine_flow_scope

--- app/controllers/routine_flow_reports_controller.rb
+++ app/controllers/routine_flow_reports_controller.rb
@@ -13,13 +13,13 @@ class RoutineFlowReportsController < ApplicationController
   private
 
   def load_routine_flows
-    @routine_flows ||= routine_flow_scope.order('completed_at desc').to_a
+    @routine_flows ||= routine_flow_scope.to_a
     @routine_flow_count = @routine_flows.size
   end
 
   def routine_flow_scope
     load_routine
-    @routine.routine_flows
+    @routine.routine_flows.order('completed_at desc')
   end
 
   def load_routine_flow
